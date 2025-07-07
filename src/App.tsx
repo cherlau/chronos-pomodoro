c8e27d7843fc4dbb21d553364bd514cae4fbd408
cherlau <cherlau.prado@appfacilita.com>
2025-07-07 15:53:53 -0300

    created task context

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,12 @@ import "./styles/theme.css";
 import "./styles/global.css";
 
 import { Home } from "./pages/Home";
-import { useState } from "react";
-import type { TaskStateModel } from "./models/TaskStateModel";
+import { TaskContextProvider } from "./contexts/TaskContext/TaskContextProvider";
 
 export function App() {
-  const initialState: TaskStateModel = {
-    tasks: [],
-    secondsRemaining: 0,
-    formattedSecondRemaining: "00:00",
-    activeTask: null,
-    currentCycle: 0,
-    config: {
-      workTime: 25,
-      shortBreakTime: 5,
-      longBreakTime: 15,
-    },
-  };
-
-  const [state, setState] = useState(initialState);
-
-  return <Home state={state} setState={setState} />;
+  return (
+    <TaskContextProvider>
+      <Home />;
+    </TaskContextProvider>
+  );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,16 +2,8 @@ import { Container } from "../../components/Container";
 import { CountDown } from "../../components/CountDown";
 import { MainForm } from "../../components/MainForm";
 import { MainTemplate } from "../../components/templates/MainTemplate";
-import type { TaskStateModel } from "../../models/TaskStateModel";
 
-type HomeProps = {
-  state: TaskStateModel;
-  setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
-};
-
-export function Home(props: HomeProps) {
-  console.log("props", props);
-
+export function Home() {
   return (
     <MainTemplate>
       <Container>
