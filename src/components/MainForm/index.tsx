476f5a3574688a2aee717792a8a702d4d3ec3bb8
cherlau <cherlau.prado@appfacilita.com>
2025-07-08 17:39:29 -0300

    Add cycle management utilities and enhance Cycles and MainForm components
    - Introduced getNextCycle and getNextCycleType utilities for cycle management.
    - Updated Cycles component to dynamically render cycle indicators based on current cycle state.
    - Enhanced MainForm to conditionally display Cycles and utilize new cycle duration logic.
    - Added formatSecondsToMinutes utility for formatting duration.

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -6,12 +6,15 @@ import { useRef } from "react";
 import type { TaskModel } from "../../models/TaskModel";
 import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 import { getNextCycle } from "../../utils/getNextCycle";
+import { getNextCycleType } from "../../utils/getNextCycleType";
+import { formatSecondsToMinutes } from "../../utils/formatSecondsToMinutes";
 
 export function MainForm() {
   const { state, setState } = useTaskContext();
   const taskNameInput = useRef<HTMLInputElement>(null);
 
   const nextCycle = getNextCycle(state.currentCycle);
+  const nextCycleType = getNextCycleType(nextCycle);
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -31,8 +34,8 @@ export function MainForm() {
       startDate: Date.now(),
       completeDate: null,
       interruptDate: null,
-      duration: 1,
-      type: "workTime",
+      duration: state.config[nextCycleType],
+      type: nextCycleType,
     };
 
     const secondsRemaining = newTask.duration * 60;
@@ -44,7 +47,7 @@ export function MainForm() {
         // aqui estou pegando o estado anterior de getNextCycle, só depois de executar a função setState() que ele vai ser refletido
         currentCycle: nextCycle,
         secondsRemaining, //// TO-DO: conferir dps
-        formattedSecondRemaining: "00:00", //// TO-DO: conferir dps
+        formattedSecondRemaining: formatSecondsToMinutes(secondsRemaining), //// TO-DO: conferir dps
         tasks: [...prevState.tasks, newTask],
       };
     });
@@ -66,9 +69,11 @@ export function MainForm() {
         <p>Lorem ipsum dolor sit amet.</p>
       </div>
 
-      <div className="formRow">
-        <Cycles />
-      </div>
+      {state.currentCycle > 0 && (
+        <div className="formRow">
+          <Cycles />
+        </div>
+      )}
 
       <div className="formRow">
         <DefaultButton icon={<PlayCircleIcon />} color="green" />
